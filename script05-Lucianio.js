93b251a53f8559930d4c82095e169903b0260aea
Pedro21h <Pedrocunhamcd@gmail.com>
2025-09-23 21:50:48 -0300

    jsaon

---
+++ script05-Lucianio.js
@@ -0,0 +1,26 @@
+const alunoDoSenai = [
+    {
+        nome:"tao",
+        email:"taofml@gmail.com",
+        matricula:"001",
+        idade:"17"
+    },
+
+    {
+        nome:"",
+        email:"",
+        matricula:"",
+        idade:""
+    },
+
+    {
+        nome:"",
+        email:"",
+        matricula:"",
+        idade:""
+    },
+]
+console.log(`Meu nome é ${alunoDoSenai[0].nome}
+            Minha matricula é ${alunoDoSenai[0].matricula}
+            Meu email é ${alunoDoSenai[0].email}
+            Minha idade é ${alunoDoSenai[0].idade}`)
---
+++ script05partedois.js
@@ -0,0 +1,8 @@
+const prompt = require("prompt-sync");
+
+const notaFinal = require("Digite uma nota: ")
+if(notaFinal >= 7){
+    console.log("Aprovado")
+}else{
+    console.log("Reprovado!")
+}
